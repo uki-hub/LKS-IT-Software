70dadc948906f2e3d148f6e605fe389c340fe75f
U K I <marzukiaslb@gmail.com>
2024-05-18 04:28:01 +0700

    commit

---
+++ project 2/DataGrid/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            var data = new List<Barang>();
+            data.Add(new Barang
+            {
+                ID = 1,
+                No = 1256,
+                Nama = "Laptop",
+                Harga = 100000,
+                Deskripsi = "Deskripsi Laptop",
+            });
+            data.Add(new Barang
+            {
+                ID = 1,
+                No = 12344,
+                Nama = "Handphone",
+                Harga = 100000,
+                Deskripsi = "Deskripsi Handphone",
+            });
+            data.Add(new Barang
+            {
+                ID = 1,
+                No = 56755,
+                Nama = "Komputer",
+                Harga = 150000,
+                Deskripsi = "Deskripsi Komputer",
+            });
+
+            InitializeComponent();
+
+            var dt = new DataTable();
+
+            dt.Columns.Add("No");
+            dt.Columns.Add("Nama Barang");
+            dt.Columns.Add("Harga Barang");
+            dt.Columns.Add("Deskripsi Barang");
+
+
+            foreach (var item in data)
+            {
+                dt.Rows.Add(item.No, item.Nama, item.Harga, item.Deskripsi);
+            }
+
+            Table.DataSource = dt;
+        }
+
+        class Barang
+        {
+            public int ID { get; set; }
+            public int No { get; set; }
+            public string Nama { get; set; }
+            public double Harga { get; set; }
+            public string Deskripsi { get; set; }
+        }
+    }
+}
